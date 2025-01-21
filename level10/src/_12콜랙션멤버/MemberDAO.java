8b26ffda9bd4a6ba0be4b4390c4408c6ee45cb51
shoot3957 <shoot3957@gmail.com>
2025-01-21 15:53:31 +0900

    Update MemberDAO.java

--- level10/src/_12콜랙션멤버/MemberDAO.java
+++ level10/src/_12콜랙션멤버/MemberDAO.java
@@ -21,41 +21,39 @@ public class MemberDAO {
         return instance;
     }
 
-    // 기존 코드: ID 중복 체크를 위한 메서드
+   
     public boolean isValidId(String id) {
         return getMemberIdxById(id) == -1;
     }
 
-    // 특정 ID로 멤버 찾기
     private int getMemberIdxById(String id) {
         for (int i = 0; i < memberList.size(); i++) {
             if (memberList.get(i).getId().equals(id)) {
                 return i;
             }
         }
-        return -1; // 찾지 못한 경우
+        return -1; 
     }
 
-    // 회원 추가
+
     public void addAMember(String id, String pw, String name) {
         memberList.add(new Member(id, pw, name));
     }
 
-    // 회원 목록 가져오기 (출력용)
     public List<Member> getAllMembers() {
         return new ArrayList<>(memberList);
     }
 
-    // 회원 검증 (ID와 비밀번호 확인)
+ 
     public boolean validateMember(String id, String pw) {
         int idx = getMemberIdxById(id);
         if (idx != -1) {
             return memberList.get(idx).getPw().equals(pw);
         }
-        return false; // 아이디가 존재하지 않거나 비밀번호가 틀림
+        return false; 
     }
 
-    // 회원 이름 업데이트
+
     public void updateMember(String id, String newName) {
         int idx = getMemberIdxById(id);
         if (idx != -1) {
@@ -63,7 +61,7 @@ public class MemberDAO {
         }
     }
 
-    // 회원 정보 출력
+
     public void printAllMember() {
         for (Member member : memberList) {
             System.out.println(member);
@@ -77,7 +75,7 @@ public class MemberDAO {
         }
     }
     public void loadFromFile(BufferedReader reader) throws IOException {
-        memberList.clear(); // 기존 회원 목록 초기화
+        memberList.clear(); 
         String line;
 
         while ((line = reader.readLine()) != null) {
@@ -93,7 +91,7 @@ public class MemberDAO {
     public void deleteMember(String id) {
         int idx = getMemberIdxById(id);
         if (idx != -1) {
-            memberList.remove(idx); // 해당 인덱스의 회원 삭제
+            memberList.remove(idx); 
         }
     }
     public boolean isDataExsit() {
